dc94da4a919d9b18ecd7a756aba3d784392659a3
korayguler <tr.koray.guler@gmail.com>
2020-11-11 01:01:16 +0300

    :bug: fix axios, gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,24 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import getPhotos from '../helper/getPhotos';
+import React from 'react';
 export default function Gallery() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    set();
-  }, []);
-
-  async function set() {
-    try {
-      const photos = await getPhotos('https://www.instagram.com/ademilter/');
-      setData(photos);
-    } catch (e) {
-      console.error('Fetching Instagram photos failed', e);
-    }
-  }
-  return (
-    <div>
-      {data.map((e) => {
-        console.log(e);
-      })}
-    </div>
-  );
+  return <div>soon</div>;
 }
