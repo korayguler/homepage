8471c237d7d71adc4d00918d6fa6b2dab9c4c5c6
Koray Güler <us.koray.guler@gmail.com>
2021-08-11 23:32:54 +0300

    route fix

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ const routes = [
   { path: '/', component: () => import('@/pages/index.vue') },
   { path: '/projects', component: () => import('@/pages/projects.vue') },
   { path: '/gallery', component: () => import('@/pages/gallery.vue') },
-  { path: '/blog', component: () => import('@/pages/blog.vue') },
 ];
 
 const config = createRouter({
